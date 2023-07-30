a31804cbec837abb53442b4a1f075f0952594082
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 14:52:59 -0600

    Initial Commit

---
+++ SAUnitTestEX/SAUnitTestEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAUnitTestEXApp.swift
+//  SAUnitTestEX
+//
+//  Created by Mitch Andrade on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct SAUnitTestEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
