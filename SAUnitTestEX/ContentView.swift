ab4a40f3cac9c0f1cbcf93ea550f3303a3bd0414
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 15:05:21 -0600

    -- Setup: How to add A Unit Test Bundle to established app 	- File/ Add Target/ Add Unit Tests Bundle

--- SAUnitTestEX/ContentView.swift
+++ SAUnitTestEX/ContentView.swift
@@ -24,3 +24,11 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Calculation {
+    func calculateTip(of enteredAmount: Double, with tip: Double) -> Double? {
+        guard enteredAmount >= 0 && tip >= 0 else { return nil }
+        let tipPercentage = tip / 100
+        return enteredAmount * tipPercentage
+    }
+}
