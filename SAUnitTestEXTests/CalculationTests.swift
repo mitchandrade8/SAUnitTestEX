48bc17f3b2d5acab939ac3c6458de9e680000d17
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-07-30 15:13:23 -0600

    -- Breakdown of, Given / When / Then 	- Check if we change something the test fails

---
+++ SAUnitTestEXTests/CalculationTests.swift
@@ -0,0 +1,30 @@
+//
+//  CalculationTests.swift
+//  SAUnitTestEXTests
+//
+//  Created by Mitch Andrade on 7/30/23.
+//
+
+import XCTest
+@testable import SAUnitTestEX
+
+final class CalculationTests: XCTestCase {
+
+    // The happy path
+    
+    func testSuccessfulTipCalculation() {
+        // Given (Arrange)
+        let enteredAmount = 100.00
+        let tipSlider = 25.00
+        let calculation = Calculation()
+        
+        // When (Act)
+        let tip = calculation.calculateTip(of: enteredAmount,
+                                           with: tipSlider)
+        
+        
+        // Then (Assert)
+        XCTAssertEqual(tip, 25)
+    }
+ 
+}
